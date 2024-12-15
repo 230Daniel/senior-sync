7bd7d7cbfb0df35b7098b211fa324b8d17d1e00f
Daniel <daniel.baynton@hotmail.com>
2024-12-15 15:42:57 +0000

    awesome animation, containers

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -13,7 +13,7 @@ createRoot(document.getElementById('root')).render(
       <Routes>
         <Route element={<Layout />}>
           <Route path="/" element={<App />} />
-          <Route path="/something" element={<h1>something</h1>} />
+          <Route path="/something" element={<h1>something, click the cool animated header to go back</h1>} />
           <Route path="*" element={<h1 >404 Not Found</h1>} />
         </Route>
       </Routes>
