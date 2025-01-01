7c20e254887c1091304b81c9da30f5aad3fd8d67
Daniel <daniel.baynton@hotmail.com>
2025-01-01 16:14:04 +0000

    Add mongo test code

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,34 @@
 
+import os
+from typing import List, Optional
 from fastapi import FastAPI
+from dotenv import load_dotenv
+from pydantic import BaseModel
+import pymongo
 
-app = FastAPI()
+load_dotenv()
 
-@app.get("/")
-def read_root():
-    return "hello, I am the backend, I speak for the trees"
+mongo = pymongo.MongoClient(os.environ["MONGO_HOST"])
+db = mongo.seniorsync
+test_collection = db.test_collection
+
+class Item(BaseModel):
+    field1: Optional[str] = None
+    field2: Optional[str] = None
+    field3: Optional[str] = None
+
+APP = FastAPI()
+
+@APP.get("/")
+async def read_root():
+    items = test_collection.count_documents({})
+    return f"hello, I am the backend, I speak for the trees. There are {items} items."
+
+@APP.get("/items")
+async def get_items() -> List[Item]:
+    return list(test_collection.find({}))
+
+@APP.post("/items")
+async def post_items(item: Item):
+    test_collection.insert_one(item.model_dump())
+    return item
