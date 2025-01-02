65f453eb7909358b34c9e0d32b2cb476ef5f91b2
Daniel <daniel.baynton@hotmail.com>
2025-01-02 14:39:33 +0000

    Remove old endpoint

--- backend/app/routers/sensors.py
+++ backend/app/routers/sensors.py
@@ -9,12 +9,12 @@ from .. import database
 router = APIRouter()
 
 
-@router.get("/", summary="Returns a list of registered sensors.")
+@router.get("", summary="Returns a list of registered sensors.")
 async def get_sensors() -> List[SensorModel]:
     return database.get_sensors()
 
 
-@router.post("/", summary="Registers a new sensor.")
+@router.post("", summary="Registers a new sensor.")
 async def add_sensor(sensor: SensorModel = Body()):
     try:
         database.add_sensor(sensor)
@@ -22,11 +22,3 @@ async def add_sensor(sensor: SensorModel = Body()):
         raise HTTPException(
             status.HTTP_409_CONFLICT, f"The {sensor.id} sensor already exists."
         )
-
-
-@router.get("/{sensor_id}", summary="Returns information on a single sensor.")
-async def get_sensor(sensor_id: str) -> SensorModel:
-    sensor = database.get_sensor(sensor_id)
-    if not sensor:
-        raise HTTPException(status.HTTP_404_NOT_FOUND, f"Sensor {sensor_id} not found.")
-    return sensor
