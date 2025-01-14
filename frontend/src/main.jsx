61e1b12ac0e36a958927f71d6eb8a9acce2c1cab
Daniel Baynton <49287178+230Daniel@users.noreply.github.com>
2025-01-14 13:49:10 +0000

    SYNC-57 Make a history view page which displays a single metric in detail (#15)
    * Query working and themed selector
    
    * Sort out range picker
    
    * fully good list
    
    * Line chart
    
    * shuffle, add get metric
    
    * Add get sensor endpoint
    
    * Fixes
    
    * refetches
    
    * remove marks, fix 404 on sensors endpoint, fix /api/, remove getRoot
    
    * Add error handling
    
    * table for text ones
    
    * Fix for themes
    
    * Add cors
    
    * Add endpoint for timestamped datapoint
    
    * Fix conversion
    
    * Sort datapoints before returning
    
    * Recharts
    
    * Some insane line graph engineering
    
    * great ticks + zoom
    
    * crappy hook
    
    * better state and tooltip
    
    * W zooming
    
    * no nulls, better ticls
    
    * Fix highlighting
    
    * style it
    
    * remove bson
    
    * Make it look nicer
    
    * always Y, horizontal grid, conditional grid
    
    * fixes

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,23 +1,45 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { QueryClientProvider } from 'react-query';
 import { BrowserRouter, Routes, Route } from "react-router";
+
+import { ThemeProvider } from './layout/ThemeSelector.jsx';
+import { QueryClient } from './api/api.js';
+
+import './index.css';
 
 import Home from './pages/Home.jsx';
 import Layout from './layout/Layout.jsx';
-
-import './index.css';
 import Metric from './pages/Metric.jsx';
+
+// REGISTER ERROR OVERLAY
+const showErrorOverlay = err => {
+  // must be within function call because that's when the element is defined for sure.
+  const ErrorOverlay = customElements.get('vite-error-overlay');
+  // don't open outside vite environment
+  if (!ErrorOverlay) { return; }
+  console.log(err);
+  const overlay = new ErrorOverlay(err);
+  document.body.appendChild(overlay);
+};
+
+window.addEventListener('error', showErrorOverlay);
+window.addEventListener('unhandledrejection', ({ reason }) => showErrorOverlay(reason));
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route element={<Layout />}>
-          <Route path="/" element={<Home />} />
-          <Route path="/metric/:metricId" element={<Metric />} />
-          <Route path="*" element={<h1 >404 Not Found</h1>} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <QueryClientProvider client={QueryClient}>
+      <ThemeProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route element={<Layout />}>
+              <Route path="/" element={<Home />} />
+              <Route path="/metric/:metricId" element={<Metric />} />
+              <Route path="*" element={<h1 >404 Not Found</h1>} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </ThemeProvider>
+    </QueryClientProvider>
   </StrictMode>
 );
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,23 +1,11 @@
-import { useEffect, useState } from "react";
-
-import { Backend, getRoot } from "../api/api";
+import { Backend } from "../api/api";
 import HealthMetrics from "../components/MetricCards";
 
 function Home() {
-
-    const [content, setContent] = useState(null);
-
-    useEffect(() => {
-        getRoot()
-            .then(text => setContent(text))
-            .catch(() => setContent("nothing, it threw an error!"));
-    }, []);
-
     return (
         <>
             <h1>Senior Sync</h1>
             <p>The backend is {Backend}.</p>
-            <p>The backend says {content ? content : "..."}</p>
             <h1 style={{ marginTop: "32px" }}>Health at a glance</h1>
             <HealthMetrics />
         </>
