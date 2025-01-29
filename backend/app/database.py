fed8ce0bc4291b1c3ed91062be0038ccc71f5c39
Daniel <daniel.baynton@hotmail.com>
2025-01-29 23:04:18 +0000

    More proper database making

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,69 +1,63 @@
 from datetime import datetime
 import os
 from typing import List, Optional
-from dotenv import load_dotenv
-import pymongo
+
+from pymongo import MongoClient
 from pymongo.collection import Collection
 
-
 from .models.datapoint import DataPoint, DataPointModels
-
 from .models.sensor import Sensor
 
-load_dotenv()
-client = pymongo.MongoClient(os.environ["MONGO_HOST"])
-db = client["senior_sync"]
+class Database:
+    def __init__(self, client: MongoClient):
+        self.client = client
+        self.db: Database = self.client["senior_sync"]
+        self.sensors: Collection = self.db["sensors"]
 
-Sensors = db["sensors"]
+    def get_datapoints_collection(self, sensor_id: str) -> Collection:
+        return self.db[f"sensor-datapoints-{sensor_id}"]
 
+    def get_sensors(self) -> List[Sensor]:
+        return [Sensor(**sensor) for sensor in self.sensors.find({})]
 
-def __get_datapoints_collection(sensor_id: str) -> Collection:
-    return db[f"sensor-datapoints-{sensor_id}"]
-
-
-def get_sensors() -> List[Sensor]:
-    return [Sensor(**sensor) for sensor in Sensors.find({})]
-
-
-def get_sensor(sensor_id: str) -> Optional[Sensor]:
-    if sensor := Sensors.find_one({"_id": sensor_id}):
-        return Sensor(**sensor)
-    return None
-
-
-def add_sensor(sensor: Sensor) -> None:
-    Sensors.insert_one(sensor.model_dump(by_alias=True))
-
-
-def add_datapoint(sensor_id: str, datapoint: DataPoint) -> None:
-    collection = __get_datapoints_collection(sensor_id)
-    collection.insert_one(datapoint.model_dump(by_alias=True))
-
-
-def get_datapoints_by_time(sensor: Sensor, start_time: datetime, end_time: datetime) -> List[DataPoint]:
-    collection = __get_datapoints_collection(sensor.id)
-    results = collection.find({"timestamp": {"$gte": start_time, "$lte": end_time}})
-    model_type = DataPointModels[sensor.value_type]
-    return [
-        model_type(**result)
-        for result in results
-    ]
-
-
-def get_current_datapoint(sensor: Sensor) -> List[DataPoint]:
-    collection = __get_datapoints_collection(sensor.id)
-    result = next(collection.find().sort({"timestamp": -1}).limit(1), None) 
-    if result is None:
+    def get_sensor(self, sensor_id: str) -> Optional[Sensor]:
+        if sensor := self.sensors.find_one({"_id": sensor_id}):
+            return Sensor(**sensor)
         return None
-    model_type = DataPointModels[sensor.value_type]
-    return model_type(**result)
 
+    def add_sensor(self, sensor: Sensor) -> None:
+        self.sensors.insert_one(sensor.model_dump(by_alias=True))
 
-def get_all_datapoints(sensor: Sensor) -> List[DataPoint]:
-    collection = __get_datapoints_collection(sensor.id)
-    results = collection.find({})
-    model_type = DataPointModels[sensor.value_type]
-    return [
-        model_type(**result)
-        for result in results
-    ]
+    def add_datapoint(self, sensor_id: str, datapoint: DataPoint) -> None:
+        collection = self.get_datapoints_collection(sensor_id)
+        collection.insert_one(datapoint.model_dump(by_alias=True))
+
+    def get_datapoints_by_time(self, sensor: Sensor, start_time: datetime, end_time: datetime) -> List[DataPoint]:
+        collection = self.get_datapoints_collection(sensor.id)
+        results = collection.find({"timestamp": {"$gte": start_time, "$lte": end_time}})
+        model_type = DataPointModels[sensor.value_type]
+        return [
+            model_type(**result)
+            for result in results
+        ]
+
+    def get_current_datapoint(self, sensor: Sensor) -> List[DataPoint]:
+        collection = self.get_datapoints_collection(sensor.id)
+        result = next(collection.find().sort({"timestamp": -1}).limit(1), None) 
+        if result is None:
+            return None
+        model_type = DataPointModels[sensor.value_type]
+        return model_type(**result)
+
+    def get_all_datapoints(self, sensor: Sensor) -> List[DataPoint]:
+        collection = self.get_datapoints_collection(sensor.id)
+        results = collection.find({})
+        model_type = DataPointModels[sensor.value_type]
+        return [
+            model_type(**result)
+            for result in results
+        ]
+
+def get_db():
+    with MongoClient(os.environ["MONGO_HOST"]) as client:
+        yield Database(client)
--- backend/app/routers/metrics.py
+++ backend/app/routers/metrics.py
@@ -1,15 +1,15 @@
 from datetime import datetime
 from operator import attrgetter
 from typing import Any, List
-from fastapi import APIRouter, Body, HTTPException, Response, status
+from fastapi import APIRouter, Body, Depends, HTTPException, Response, status
 from fastapi.exceptions import RequestValidationError
 from pydantic import ValidationError
 import pandas as pd
-from fastapi.responses import StreamingResponse # Add to Top
+from fastapi.responses import StreamingResponse
 from fastapi.encoders import jsonable_encoder
 
+from ..database import Database, get_db
 from ..models.datapoint import CreateDataPoint, DataPoint, ColourDataPoint, DataPointModels
-from .. import database
 from ..models.sensor import SensorWithDatapoint
 
 router = APIRouter()
@@ -20,9 +20,9 @@ router = APIRouter()
     summary="Records the the reading of a sensor with a timestamp.",
     status_code=status.HTTP_201_CREATED,
 )
-async def record(sensor_id: str, data_point: CreateDataPoint = Body()):
+async def record(sensor_id: str, data_point: CreateDataPoint = Body(), db: Database = Depends(get_db)):
 
-    if not (sensor := database.get_sensor(sensor_id)):
+    if not (sensor := db.get_sensor(sensor_id)):
         raise HTTPException(status.HTTP_404_NOT_FOUND, f"Sensor {sensor_id} not found.")
 
     try:
@@ -53,33 +53,38 @@ async def record(sensor_id: str, data_point: CreateDataPoint = Body()):
     except ValidationError as exc:
         raise RequestValidationError(exc.errors())
 
-    database.add_datapoint(sensor.id, data_point)
+    db.add_datapoint(sensor.id, data_point)
 
     return Response(status_code=status.HTTP_201_CREATED)
 
 
 @router.get("/{sensor_id}/history", summary="Fetches all datapoints for a sensor within a given time range.")
-async def get_history(sensor_id: str, start_time: datetime, end_time: datetime = None) -> List[DataPoint]:
+async def get_history(
+    sensor_id: str, 
+    start_time: datetime, 
+    end_time: datetime = None, 
+    db: Database = Depends(get_db)
+    ) -> List[DataPoint]:
     end_time = end_time or datetime.now()
 
-    if not (sensor := database.get_sensor(sensor_id)):
+    if not (sensor := db.get_sensor(sensor_id)):
         raise HTTPException(status.HTTP_404_NOT_FOUND,
                             f"Sensor {sensor_id} not found.")
 
-    data_points = database.get_datapoints_by_time(sensor, start_time, end_time)
+    data_points = db.get_datapoints_by_time(sensor, start_time, end_time)
     data_points.sort(key=attrgetter("timestamp"))
     return data_points
 
 
 
 @router.get("/all", summary="Fetches current metrics from a sensor.")
-async def get_metrics() -> List[SensorWithDatapoint]:
+async def get_metrics(db: Database = Depends(get_db)) -> List[SensorWithDatapoint]:
     
-    sensors = database.get_sensors()
+    sensors = db.get_sensors()
     modelList = []
 
     for sensor in sensors:
-        data_point = database.get_current_datapoint(sensor)
+        data_point = db.get_current_datapoint(sensor)
         model = SensorWithDatapoint(value = data_point, **sensor.model_dump(by_alias=True))
         modelList.append(model)
     
@@ -89,13 +94,13 @@ async def get_metrics() -> List[SensorWithDatapoint]:
 
     
 @router.get("/{sensor_id}/export", summary="Creates a CSV file with all datapoints from this sensor.")
-async def get_export(sensor_id: str) -> StreamingResponse:
+async def get_export(sensor_id: str, db: Database = Depends(get_db)) -> StreamingResponse:
    
-    if not (sensor := database.get_sensor(sensor_id)):
+    if not (sensor := db.get_sensor(sensor_id)):
         raise HTTPException(status.HTTP_404_NOT_FOUND,
                             f"Sensor {sensor_id} not found.")
 
-    data_points = database.get_all_datapoints(sensor)
+    data_points = db.get_all_datapoints(sensor)
     data_points.sort(key=attrgetter("timestamp"))
     df = pd.DataFrame((jsonable_encoder(data_points))
         
@@ -105,5 +110,3 @@ async def get_export(sensor_id: str) -> StreamingResponse:
         media_type="text/csv",
         headers={"Content-Disposition": f"attachment; filename=Export_{sensor_id}.csv"}
 )
-
-
