3ec02d12897bce63fef2459f9d98a0958a9761f6
FxG28 <finlay.gerchen.025@cranfield.ac.uk>
2025-01-29 21:55:53 +0000

    Footer

---
+++ frontend/src/layout/Footer.jsx
@@ -0,0 +1,6 @@
+const Footer = () => {
+  
+    return <footer>{`Senior Sync securely integrates health data from connected wearables, ensuring privacy and compliance with GDPR and the UK Data Protection Act 2018. Data is shared only with permitted parties. Users have full control over their data, including the right to access, correct, and delete. For inquiries, contact our Data Protection team here. To request data deletion, please visit our Right to Be Forgotten Form.`}</footer>;
+  };
+  
+  export default Footer;
