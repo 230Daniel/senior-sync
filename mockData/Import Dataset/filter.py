3f46e3cb4e2adeba2dbba3075a8eb2958d1a0404
jc11 <jonny.coulson@roke.co.uk>
2025-01-30 18:30:41 +0000

    Added better filter and sent to endpoint

--- mockData/Import Dataset/filter.py
+++ mockData/Import Dataset/filter.py
@@ -1,27 +1,23 @@
 import json
+from datetime import datetime
 
 with open ("Output.json","r") as file:
     records=json.load(file)
 
-print("\n".join(set([record["type"] for record in records])))
+for sensor_type in ["HKQuantityTypeIdentifierOxygenSaturation","QuantityTypeIdentifierVO2Max","HKQuantityTypeIdentifierRespiratoryRate"]:
+    datapoints=[]
 
-datapoints=[]
+    for record in records:
+        if record["startDate"] > "2020" and record["type"] == sensor_type:
+            datapoint={
+                "value":record["value"], 
+                "timestamp":datetime.strptime(record["startDate"],"%Y-%m-%d %H:%M:%S %z").isoformat()
+            }
+            datapoints.append(datapoint)
 
-for record in records:
-   if record["startDate"] > "2024" and record["type"] == "HKQuantityTypeIdentifierOxygenSaturation":
-       datapoint={
-           "value":record["value"], 
-           "timestamp":record["startDate"]
-       }
-       datapoints.append(datapoint)
+    with open(f"{sensor_type}.json","w") as file:
+        json.dump(datapoints,file, indent=4)
 
-with open("o2.json","w") as file:
-    json.dump(datapoints,file, indent=4)
 
-# HKQuantityTypeIdentifierWalkingHeartRateAverage
-# HKQuantityTypeIdentifierHeartRate
-# HKQuantityTypeIdentifierOxygenSaturation
-# HKQuantityTypeIdentifierHeartRate
-# HKQuantityTypeIdentifierRestingHeartRate
-# HKQuantityTypeIdentifierRespiratoryRate
-# HKQuantityTypeIdentifierVO2Max
+
+   
--- mockData/Import Dataset/send.py
+++ mockData/Import Dataset/send.py
@@ -1,7 +1,12 @@
 import json
 import requests
 
-with open ("o2.json","r") as file:
+with open ("HKQuantityTypeIdentifierOxygenSaturation.json","r") as file:
     datapoints=json.load(file)
+response=requests.post("http://localhost:8000/metrics/o2/mass",json=datapoints)
+print(response.text)
 
-response=requests.post
+with open ("HKQuantityTypeIdentifierRespiratoryRate.json","r") as file:
+    datapoints=json.load(file)
+response=requests.post("http://localhost:8000/metrics/o2/mass",json=datapoints)
+print(response.text)
