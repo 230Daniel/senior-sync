b54aa49f6b1f33d0379c34e0496fb10f1032607a
Umar Sulaiman <umar.sulaiman@roke.co.uk>
2025-01-30 11:38:47 +0000

    Fixed Model

--- backend/app/routers/metrics.py
+++ backend/app/routers/metrics.py
@@ -112,7 +112,7 @@ async def get_export(sensor_id: str) -> StreamingResponse:
     summary="Records mass test data import.",
     status_code=status.HTTP_201_CREATED,
 )
-async def mass_import(sensor_id: str, data_points: List[BaseDataPointModel] = Body()):
+async def mass_import(sensor_id: str, data_points: List[CreateDataPoint] = Body()):
 
     if not (sensor := database.get_sensor(sensor_id)):
         raise HTTPException(status.HTTP_404_NOT_FOUND, f"Sensor {sensor_id} not found.")
