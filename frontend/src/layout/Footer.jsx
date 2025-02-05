f27b81c2638053ba35ca3a1de5333c980dafc8ba
FxG28 <finlay.gerchen.025@cranfield.ac.uk>
2025-02-05 21:34:30 +0000

    Footer element (#32)
    * Footer
    
    * changes
    
    * changes
    
    * Changes

---
+++ frontend/src/layout/Footer.jsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+  
+    return <footer>
+      
+      Senior Sync securely integrates health data from connected wearables, ensuring privacy and compliance with GDPR
+      and the UK Data Protection Act 2018. Data is shared only with permitted parties.
+      Users have full control over their data, including the right to access, correct, and delete. 
+      For inquiries, contact our Data Protection team <a href="mailto:khadija.khan.267@cranfield.ac.uk?subject=Data%20inquiry.">here</a>. To request data deletion, please visit our Right to Be Forgotten Form.
+      
+        </footer>;
+  };
+  
+  export default Footer;
