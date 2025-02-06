07791f15c6f8b18aee3598b62fcad149b9f412b1
jc11 <jonny.coulson@roke.co.uk>
2025-02-06 09:57:05 +0000

    changed origin of URL

--- mockData/Import Dataset/send.py
+++ mockData/Import Dataset/send.py
@@ -3,20 +3,20 @@ import requests
 
 with open ("HKQuantityTypeIdentifierOxygenSaturation.json","r") as file:
     datapoints=json.load(file)
-response=requests.post("http://localhost:8000/metrics/o2/mass",json=datapoints)
+response=requests.post("https://utili.xyz:8443/api/metrics/o2/mass",json=datapoints)
 print(response.text)
 
 with open ("HKQuantityTypeIdentifierHeartRate.json","r") as file:
     datapoints=json.load(file)
-response=requests.post("http://localhost:8000/metrics/HKQuantityTypeIdentifierHeartRate/mass",json=datapoints)
+response=requests.post("https://utili.xyz:8443/api/metrics/HKQuantityTypeIdentifierHeartRate/mass",json=datapoints)
 print(response.text)
 
 with open ("HKQuantityTypeIdentifierRespiratoryRate.json","r") as file:
     datapoints=json.load(file)
-response=requests.post("http://localhost:8000/metrics/HKQuantityTypeIdentifierRespiratoryRate/mass",json=datapoints)
+response=requests.post("https://utili.xyz:8443/api/metrics/HKQuantityTypeIdentifierRespiratoryRate/mass",json=datapoints)
 print(response.text)
 
 with open ("HKQuantityTypeIdentifierRestingHeartRate.json","r") as file:
     datapoints=json.load(file)
-response=requests.post("http://localhost:8000/metrics/HKQuantityTypeIdentifierRestingHeartRate/mass",json=datapoints)
+response=requests.post("https://utili.xyz:8443/api/metrics/HKQuantityTypeIdentifierRestingHeartRate/mass",json=datapoints)
 print(response.text)
